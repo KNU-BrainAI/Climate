fa331b743e94177194089b040c516e3ab3a5a36c
Eunchan Lee <46081500+purang2@users.noreply.github.com>
2021-07-29 18:25:08 +0900

    Create Dense-ReLU-Dense.py

---
+++ Dense-ReLU-Dense.py
@@ -0,0 +1,33 @@
+class TFBertClassifier(tf.keras.Model):
+    def __init__(self, model_name, dir_path, num_class):
+        super(TFBertClassifier, self).__init__()
+
+        self.bert = TFBertModel.from_pretrained(model_name, cache_dir=dir_path)
+        self.dropout = tf.keras.layers.Dropout(self.bert.config.hidden_dropout_prob)
+        '''
+        D_in, H, D_out = 
+        self.classifier = tf.keras.layers.Dense(num_class, 
+                                                kernel_initializer=tf.keras.initializers.TruncatedNormal(self.bert.config.initializer_range), 
+                                                name="classifier")
+        '''
+        model_eunchan = tf.keras.models.Sequential()
+        model_eunchan.add(tf.keras.layers.Dense(100, 
+                                                kernel_initializer=tf.keras.initializers.TruncatedNormal(self.bert.config.initializer_range), 
+                                                name="classifier1"))
+        model_eunchan.add(tf.keras.layers.ReLU())
+        model_eunchan.add(tf.keras.layers.Dense(num_class, 
+                                                kernel_initializer=tf.keras.initializers.TruncatedNormal(self.bert.config.initializer_range), 
+                                                name="classifier2"))
+        
+        self.classifier = model_eunchan
+        
+    def call(self, inputs, attention_mask=None, token_type_ids=None, training=False):
+        
+        #outputs 값: # sequence_output, pooled_output, (hidden_states), (attentions)
+        outputs = self.bert(inputs, attention_mask=attention_mask, token_type_ids=token_type_ids)
+        pooled_output = outputs[1] 
+        pooled_output = self.dropout(pooled_output, training=training)
+        logits = self.classifier(pooled_output)
+
+        return logits
+    
